1f3516e110ce531a46d1045826da953194648a6e
victorlomi <victorlominyo@gmail.com>
2020-12-16 05:55:39 +0300

    update model with User, Post, and Comment

--- app/models.py
+++ app/models.py
@@ -9,38 +9,36 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    pitches = db.relationship('Pitch', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.username}>'
 
-class Pitch(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     category = db.Column(db.String(64))
-    comments = db.relationship('Comment', backref='pitch', lazy='dynamic')
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
     def __repr__(self):
-        return f'<Pitch {self.id}>'
+        return f'<Post {self.id}>'
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(200))
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitch.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
 
     def __repr__(self):
         return f'<Comment {self.id}>'
-    
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
