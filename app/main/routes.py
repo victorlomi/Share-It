3221539a904f8ee1555900005e8cbbd8bc8688ce
victorlomi <victorlominyo@gmail.com>
2020-12-16 07:28:30 +0300

    style the post in the homepage

--- app/main/routes.py
+++ app/main/routes.py
@@ -19,9 +19,9 @@ def add_post():
     form = forms.AddForm()
 
     if form.validate_on_submit():
-        # pitch = Pitch(author=current_user, body=form.body.data, category=form.category.data)
-        # db.session.add(pitch)
-        # db.session.commit()
+        post = Post(author=current_user, title=form.title.data, body=form.body.data)
+        db.session.add(post)
+        db.session.commit()
         return redirect(url_for('main.index'))
 
     return render_template('add_post.html', form=form)
