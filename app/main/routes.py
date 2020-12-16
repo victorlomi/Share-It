08f3990fd247018b8fae045a52f91dfb54f52c04
victorlomi <victorlominyo@gmail.com>
2020-12-16 09:41:15 +0300

    add quotes api to project and show the quote on the homepage as quote of the day

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,13 +2,19 @@ from flask import render_template, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user
 from app.main import bp
 from app import db
+import requests
 from app.models import User, Post, Comment
 from app.main import forms
 
 @bp.route('/')
 def index():
+    # get the quote of the day
+    url = "http://quotes.stormconsultancy.co.uk/random.json"
+    response = requests.get(url).json()
+
+    # get all the posts for displaying
     posts = Post.query.all()
-    return render_template('index.html', posts=posts, User=User)
+    return render_template('index.html', posts=posts, User=User, quote=response)
 
 @bp.route('/post/<post_id>', methods=['GET', 'POST'])
 def post(post_id):
